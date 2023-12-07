67a24a7429212852f16bc5e4824b6000e3e76635
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2023-12-07 14:43:34 -0500

    Add imports and setup.

--- HasPrimitives/Basic.lean
+++ HasPrimitives/Basic.lean
@@ -1,9 +1,15 @@
 import Mathlib.Analysis.Complex.Basic
 import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
+import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 
+set_option autoImplicit false
+
+open Set Complex Real Metric Topology
 
 -- From V. Beffara https://github.com/vbeffara/RMT4
 def has_primitives (U : Set ℂ) : Prop :=
   ∀ f : ℂ → ℂ, DifferentiableOn ℂ f U → ∃ g : ℂ → ℂ, DifferentiableOn ℂ g U ∧ Set.EqOn (deriv g) f U
 
-
+lemma has_primitives_of_simply_connected (U : Set ℂ) (hSc : SimplyConnectedSpace U) (hO : IsOpen U) :
+   has_primitives U := by sorry
