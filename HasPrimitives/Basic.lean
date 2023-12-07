c139b46d51efb69fef9465986b0f6ead15c16a7b
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-12-07 17:27:45 -0500

    Comments

--- HasPrimitives/Basic.lean
+++ HasPrimitives/Basic.lean
@@ -5,17 +5,19 @@ import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 
 set_option autoImplicit false
 
-open Set Complex Real Metric Topology
-
 -- From V. Beffara https://github.com/vbeffara/RMT4
 def has_primitives (U : Set ℂ) : Prop :=
   ∀ f : ℂ → ℂ, DifferentiableOn ℂ f U → ∃ g : ℂ → ℂ, DifferentiableOn ℂ g U ∧ Set.EqOn (deriv g) f U
 
-lemma has_primitives_of_simply_connected (U : Set ℂ) (hSc : SimplyConnectedSpace U) (hO : IsOpen U) :
+-- main theorem: holomorphic functions on simply connected open sets have primitives
+theorem has_primitives_of_simply_connected (U : Set ℂ) (hSc : SimplyConnectedSpace U) (hO : IsOpen U) :
    has_primitives U := by sorry
 
 namespace Real
 
+-- A useful function: goes from z₁ to z₂ with a speed that vanishes at the endpoints
+-- Having a vanishing speed at the endpoints allows paths that are differentiable by parts to be
+--  parametrized in a differentiable way
 noncomputable def straightSeg (t₁ t₂ : ℝ ) (ht : t₁ < t₂) (z₁ z₂ : ℂ) : ℝ → ℂ :=
   fun t => z₁ * (1 - cos (t * π / (t₂ - t₁))) + z₂ * cos (t * π / (t₂ - t₁))
 
