7dd0318f018c22a89e12cd056c157e85d5b72d1d
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-12-07 17:28:34 -0500

    Capitalization

--- HasPrimitives/Basic.lean
+++ HasPrimitives/Basic.lean
@@ -6,12 +6,12 @@ import Mathlib.AlgebraicTopology.FundamentalGroupoid.SimplyConnected
 set_option autoImplicit false
 
 -- From V. Beffara https://github.com/vbeffara/RMT4
-def has_primitives (U : Set ℂ) : Prop :=
+def hasPrimitives (U : Set ℂ) : Prop :=
   ∀ f : ℂ → ℂ, DifferentiableOn ℂ f U → ∃ g : ℂ → ℂ, DifferentiableOn ℂ g U ∧ Set.EqOn (deriv g) f U
 
 -- main theorem: holomorphic functions on simply connected open sets have primitives
-theorem has_primitives_of_simply_connected (U : Set ℂ) (hSc : SimplyConnectedSpace U) (hO : IsOpen U) :
-   has_primitives U := by sorry
+theorem hasPrimitivesOfSimplyConnected (U : Set ℂ) (hSc : SimplyConnectedSpace U) (hO : IsOpen U) :
+   hasPrimitives U := by sorry
 
 namespace Real
 
