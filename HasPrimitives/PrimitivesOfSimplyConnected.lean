b967d2e4dc281672c5bdecd771b2f31adddddd43
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2023-12-19 11:59:23 -0500

    curvInt_eval_of_primitive

--- HasPrimitives/PrimitivesOfSimplyConnected.lean
+++ HasPrimitives/PrimitivesOfSimplyConnected.lean
@@ -49,13 +49,22 @@ theorem intervalIntegral.integral_eq_sub_of_hasDerivAt_of_le' {E : Type*} [Norme
 
 
 theorem curvInt_eval_of_primitive {t₁ t₂ : ℝ} (ht : t₁ ≤ t₂) {γ : ℝ → ℂ} {f F : ℂ → ℂ} {U : Set ℂ}
-    (U_open : IsOpen U) (γ_in_U : ∀ t, t ∈ [[t₁, t₂]] → γ t ∈ U)
-    (F_holo : DifferentiableOn ℂ F U)
-    (F_prim : Set.EqOn (deriv F) f U)
-    (hγ : DifferentiableOn ℝ γ ([[t₁, t₂]])) :
+    (U_open : IsOpen U) (γ_in_U : ∀ t, t ∈ Set.Icc t₁ t₂ → γ t ∈ U)
+    (F_prim : ∀ z ∈ U, HasDerivAt F (f z) z)
+    (hγ : DifferentiableOn ℝ γ (Set.Ioo t₁ t₂)) :
     CurvInt t₁ t₂ f γ = F (γ t₂) - F (γ t₁) := by
-  dsimp [CurvInt]
-  apply intervalIntegral.integral_eq_sub_of_hasDerivAt_of_le
+  have cont₁ : ∀ t ∈ Set.Icc t₁ t₂, ContinuousWithinAt (fun {t₂} => F (γ t₂)) (Set.Icc t₁ t₂) t := sorry
+  have cont₂ : ∀ t ∈ [[t₁, t₂]], ContinuousWithinAt (fun y => deriv γ y • f (γ y)) [[t₁, t₂]] t := sorry
+  refine intervalIntegral.integral_eq_sub_of_hasDerivAt_of_le ht (f := F ∘ γ)
+    (fun t ht' ↦ cont₁ t ht') ?_ (ContinuousOn.intervalIntegrable (fun t ht' ↦ cont₂ t ht'))
+  intro t ht'
+  convert HasDerivAt.comp (h₂ := F) (h := γ) (x := t) (h₂' := f (γ t)) (h' := deriv γ t) (F_prim (γ t)
+    (γ_in_U t (Set.mem_Icc_of_Ioo ht'))) ?_ using 1
+  · simp only [smul_eq_mul]
+    ring
+  · rw [hasDerivAt_deriv_iff]
+    exact (hγ t ht').differentiableAt
+      (mem_nhds_iff.mpr ⟨Set.Ioo t₁ t₂, Eq.subset rfl, isOpen_Ioo, ht'⟩)
 
 
 #exit
